5c693eb34969aff78e6ed3cd7542a6c309a4c981
Mortef <m.ortego.f@gmail.com>
2022-02-12 19:15:59 +0100

    Structure folders by type

---
+++ lib/constants/assets_path.dart
@@ -0,0 +1,5 @@
+const String alarmIcon = 'assets/images/alarm.png';
+const String ambulanceIcon = 'assets/images/ambulance.png';
+const String firetruckIcon = 'assets/images/firetruck.png';
+const String panicButtonIcon = 'assets/images/panic_button.png';
+const String policeIcon = 'assets/images/police.png';
